79201a741a8d8b29ccd14adb6c4935288202e9c1
marie.Fonkou <marie.fonkou@virginpulse.com>
2016-11-16 08:56:40 -0500

    Added log in and sign out functionalities and handle authentication errors.

---
+++ RedEye/Enum.swift
@@ -0,0 +1,9 @@
+//
+//  Enum.swift
+//  RedEye
+//
+//  Created by Marie Fonkou on 11/14/16.
+//  Copyright © 2016 Marie Fonkou. All rights reserved.
+//
+
+import Foundation
---
+++ RedEye/UserInterface.swift
@@ -0,0 +1,9 @@
+//
+//  UserInterface.swift
+//  RedEye
+//
+//  Created by Marie Fonkou on 11/14/16.
+//  Copyright © 2016 Marie Fonkou. All rights reserved.
+//
+
+import Foundation
