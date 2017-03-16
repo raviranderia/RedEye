f9c8bf5b21d436f188f428d853a773fd17bced92
raviranderia <ravi.randeria@nyu.edu>
2017-03-15 21:03:24 -0400

    Update DriverLoginController.swift

--- RedEye/DriverLoginController.swift
+++ RedEye/DriverLoginController.swift
@@ -11,49 +11,26 @@ import Firebase
 
 class DriverLoginController: UIViewController {
     
+    // MARK: IBOutlets
     @IBOutlet weak var driverEmailAddressField: TextField!
-    
     @IBOutlet weak var driverPasswordField: TextField!
-
     @IBOutlet weak var loginBtnPressed: LoginButton!
     
+    // MARK: Variables
     var driverEmailAddressList = [String]()
-    
     var driverEmailAddressAndUniqueId = [String : String]()
-    
     var driverIds = [String]()
-    
     var driverUid: String!
-    
     var emailAddress: String = ""
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
-        
         getAllDriversEmailAddresses()
-        
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        
-//        if (FIRAuth.auth()?.currentUser?.uid) != nil{
-//            
-//            var appDelegate: AppDelegate
-//            appDelegate = UIApplication.shared.delegate as! AppDelegate
-//            
-//            appDelegate.window?.rootViewController
-//                = self.storyboard?.instantiateViewController(withIdentifier: "driverTabBarController")
-//            
-//            
-//        }
-        
-         hideKeyboardWhenTappedView()
-
-        
-        
-   
-        
+        hideKeyboardWhenTappedView()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -61,12 +38,13 @@ class DriverLoginController: UIViewController {
          saveDriverUid()
     }
     
-
+    // get rid of this function if not using it
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    // What does this function do?
     func getAllDriversEmailAddresses(){
         
         
@@ -74,10 +52,12 @@ class DriverLoginController: UIViewController {
     
     }
     
+    // remove extra lines
+    // if you want to follow MVVM architecture...your network calls are made from the view model...create a seperate file called DriverLoginViewModel and create a driverLoginViewModel variable in this controller and make it perform network requests
+    // driverEmailAddressList should be a variable in the viewModel
+    // basically the view controller should only contain code to display things to UI..no logic or processing happens in the view controller..everything is handled by the viewModel
+    
     func checkDriverByEmail(driverEmail: String) {
-        
-        
-        
         FIRDatabase.database().reference().child("Drivers").observe(.childAdded, with: { (snapshot) in
             
             print("snapshot \(snapshot)")
@@ -85,7 +65,7 @@ class DriverLoginController: UIViewController {
                 
                 let key = snapshot.key
                 self.driverIds.append(key)
-                
+                // this is a magic value...create a variable for this in viewModel called driverEmailAddresKey
                 if let driverEmailAddress = dictionary["driverEmailAddress"] as? String {
                     self.emailAddress = driverEmailAddress
                 }
@@ -98,11 +78,11 @@ class DriverLoginController: UIViewController {
             if self.driverEmailAddressList.contains(driverEmail) {
                 
                 self.driverUid = self.driverEmailAddressAndUniqueId["\(driverEmail)"]
-                
+                // magic value..store segue identifer as variable in view model
                 self.performSegue(withIdentifier: "goToDriverProfile", sender: nil)
                 
             } else {
-                
+                // create a common function to display alert controller and simply pass title and message string to it...which also should be defined as variables in viewModel
                 let alertControllerVerificationEmail = UIAlertController (title : "Email address unknown", message: "You need to enter your driver information" , preferredStyle: .alert)
                 let actionVerification = UIAlertAction (title: "OK", style: . cancel, handler : nil)
                 alertControllerVerificationEmail.addAction(actionVerification)
@@ -118,20 +98,18 @@ class DriverLoginController: UIViewController {
     
     @IBAction func driverLoginBtnPressed(_ sender: Any) {
         if self.driverEmailAddressField.text == "" || self.driverPasswordField.text == "" {
-            
+            // common function to avoid repeating code
             let alertControllerLogin = UIAlertController (title : "Oops, you were too fast", message: "Please make sure to fill in every field.", preferredStyle: .alert)
             let actionSignUp = UIAlertAction (title: "OK", style: . cancel, handler : nil)
             alertControllerLogin.addAction(actionSignUp)
             self.present(alertControllerLogin, animated: true, completion: nil)
             
         } else{
-            
+            // network logic goes in viewModel
             FIRAuth.auth()?.signIn(withEmail: driverEmailAddressField.text!, password: driverPasswordField.text!, completion: { (user, error) in
                 
                 if error != nil {
-                    
-                    // create
-                    
+                     // common function to avoid repeating code
                     let alertControllerSignUp = UIAlertController (title : "Come on, you know this!", message: "The password you entered doesn't match the one we have in record." , preferredStyle: .alert)
                     let actionSignUp = UIAlertAction (title: "OK", style: . cancel, handler : nil)
                     alertControllerSignUp.addAction(actionSignUp)
@@ -146,6 +124,8 @@ class DriverLoginController: UIViewController {
                             }
                         })
                     } else {
+                                    // common function to avoid repeating code
+
                         let alertControllerVerificationEmail = UIAlertController (title : "Email address unknown", message: "You need to enter your driver information" , preferredStyle: .alert)
                         let actionVerification = UIAlertAction (title: "OK", style: . cancel, handler : nil)
                         alertControllerVerificationEmail.addAction(actionVerification)
@@ -169,7 +149,7 @@ class DriverLoginController: UIViewController {
     
     func login(){
         if self.driverEmailAddressField.text == "" || self.driverPasswordField.text == "" {
-            
+                        // common function to avoid repeating code
             let alertControllerLogin = UIAlertController (title : "Oops, you were too fast", message: "Please make sure to fill in every field.", preferredStyle: .alert)
             let actionSignUp = UIAlertAction (title: "OK", style: . cancel, handler : nil)
             alertControllerLogin.addAction(actionSignUp)
@@ -209,9 +189,7 @@ class DriverLoginController: UIViewController {
                 
             }
         })
-    }
-    
-    
+    }    
 }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -220,17 +198,13 @@ class DriverLoginController: UIViewController {
             let navController = tabBarController.viewControllers![0] as! UINavigationController
             let destinationVC = navController.topViewController as! DriverProfileController
             destinationVC.driverUid = self.driverUid!
-          
         }
     }
     
     func saveDriverUid(){
         let defaults = UserDefaults.standard
         defaults.set(driverUid, forKey: "driverEmailAddress")
-    }
-    
-    
-    
+    }    
     
     func hideKeyboardWhenTappedView (){
         let tap = UITapGestureRecognizer(target: self, action:#selector(LoginController.hideKeyBoard))
